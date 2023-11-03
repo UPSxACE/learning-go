68f1c524a10721fe727bd0c29f6376eac3936203
Eduardo Botelho <eduardo11224b@gmail.com>
2023-11-03 09:08:13 +0000

    testing math random and slices on greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,17 @@
 package greetings
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
+/*
+This function takes a name parameter whose type is string. The function also returns a string.
+In Go, a function whose name starts with a capital letter can be called by a function not in the same package.
+This is known in Go as an exported name.
+*/
 func Hello(name string) (string, error){
 
 	if(name == ""){
@@ -13,19 +19,26 @@ func Hello(name string) (string, error){
 	}
 	
 	// Return a greeting that embeds the name in a message.
-	msg := fmt.Sprintf("Hi, %v. Welcome!", name)
+	msg := fmt.Sprintf(randomBaseText(), name)
+	/*
+	In Go, the := operator is a shortcut for declaring and initializing a variable in one line (Go uses the value on the right to determine the variable's type). Taking the long way, you might have written this as:
+
+	var message string
+	message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	*/
 	return msg, nil
 }
 
-/*
-This function takes a name parameter whose type is string. The function also returns a string.
-In Go, a function whose name starts with a capital letter can be called by a function not in the same package.
-This is known in Go as an exported name. 
-*/
+func randomBaseText() string{
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
 
-/*
-In Go, the := operator is a shortcut for declaring and initializing a variable in one line (Go uses the value on the right to determine the variable's type). Taking the long way, you might have written this as:
+	// Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
+}
 
-var message string
-message = fmt.Sprintf("Hi, %v. Welcome!", name)
-*/
