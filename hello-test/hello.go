e57bed412b62d2cd76ef45bcc277baa0989381c1
Eduardo Botelho <eduardo11224b@gmail.com>
2023-11-03 09:07:16 +0000

    testing slices and math random in greetings

--- hello-test/hello.go
+++ hello-test/hello.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"upsxace/greetings"
 )
 
 func main(){
-	message := greetings.Hello("Ace")
+ 	// Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+	message, err := greetings.Hello("Ace")
+	// If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+
+
+
 	fmt.Println(message)
 }
 
